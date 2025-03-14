400fa3d427e88cb4c9e951e61f9a03942c6c986b
Funkz1984 <benoliverduncan@gmail.com>
2025-03-14 17:41:06 +0000

    Base for API
    Takes API information. Currently does not do everything the project demands.

---
+++ Main code.py
@@ -0,0 +1,35 @@
+import requests
+
+# get API URL and do things with it
+# e.g. https://api.github.com/users/kamranahmedse/events
+
+base_url = "https://api.github.com/users/"
+
+def get_api_url(username):
+    url = f"{base_url}{username}/events"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        git_data = response.json()
+        return git_data
+    else:
+        print("Error: ", response.status_code)
+
+    
+
+def main():
+    opening = "Hello, please enter the GitHub Username: "
+    username = input(opening)
+    
+    test = get_api_url(username)
+    if test:
+        for event in test:
+            print(event['type'])
+            print(event['url'])
+    else:
+        print("No data found")
+
+
+
+if __name__ == "__main__":
+    main()
