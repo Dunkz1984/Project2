9752934467b86f4c720fe4cec6f6762aee7ea9d9
Funkz1984 <200074515+Dunkz1984@users.noreply.github.com>
2025-03-17 19:19:28 +0000

    New main code
    The number of the tasks has now been located, so that each type prints as "PushEvent: 3," which is nice. The task will now be to format it all correctly so that it becomes "Pushed 3 commits to URL"

--- Main code.py
+++ Main code.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 # get API URL and do things with it
 # e.g. https://api.github.com/users/kamranahmedse/events
@@ -15,7 +16,18 @@ def get_api_url(username):
     else:
         print("Error: ", response.status_code)
 
+
+def collate(events):
+    listed = set(events)
+    for event in listed:
+        count = sum(1 for e in events if e == event)
+        #Had to look this up, but this is a good way of finding out the amount of times 
+        #an even occurs. Now I need to find a way to print this out so that it looks like:
+        # - Pushed 3 commmits to URL.
+        #Hmm
+        print(f"{event}: {count}")
     
+
 
 def main():
     opening = "Hello, please enter the GitHub Username: "
@@ -23,11 +35,18 @@ def main():
     
     test = get_api_url(username)
     if test:
-        for event in test:
-            print(f"Event: {event['type']}")
-            print(f"URL: {event['repo']['url']}\n")
+        event_list = [event['type'] for event in test]
+        collate(event_list)
+        
+
+            #print(f"Event: {event['type']}")
+            #print(f"URL: {event['repo']['url']}\n")
     else:
         print("No data found")
+
+    # So far, this creates a program that takes a username and returns the events and url of 
+    # the user. This is OK, but I now need to make a list of the things that are happening
+    # and then collate them into a much easier to read format. OK.
 
 
 
